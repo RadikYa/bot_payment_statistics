26f4cfc32e8ccf50c6a32d0a4050d884693476b2
Radik Yaushev <sokkomp@mail.ru>
2025-01-27 14:01:11 +0300

    Add logs

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, request
 import requests
 import os
+import logging
 
+# Создание Flask приложения
 app = Flask(__name__)
 
+# Получение переменных окружения
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SPREADSHEET_SCRIPT_URL = os.getenv("SPREADSHEET_SCRIPT_URL")
+
+# Настройка логирования
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route("/")
 def home():
@@ -13,17 +19,24 @@ def home():
 
 @app.route(f"/{BOT_TOKEN}", methods=["POST"])
 def telegram_webhook():
-    data = request.json  # Исправлено 'grequest' на 'request'
-    if not data:
-        return "No data received", 400
+    try:
+        data = request.json
+        if not data:
+            logging.error("No data received")
+            return "No data received", 400
 
-    # Проверяем, содержит ли сообщение 'to'lov:'
-    message = data.get("message", {}).get("text", "")
-    if "to'lov:" in message:
-        requests.post(SPREADSHEET_SCRIPT_URL, json={"message": message})
-    return "OK", 200
+        # Проверка наличия текста в сообщении
+        message = data.get("message", {}).get("text", "")
+        if "to'lov:" in message:
+            # Отправка сообщения в Google Apps Script
+            requests.post(SPREADSHEET_SCRIPT_URL, json={"message": message})
+        return "OK", 200
+    except Exception as e:
+        # Логирование ошибок
+        logging.exception(f"An error occurred: {e}")
+        return "Internal Server Error", 500
 
 if __name__ == "__main__":
-    # Локальный запуск
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+
 
